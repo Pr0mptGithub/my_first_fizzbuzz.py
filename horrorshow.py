75edace099feb75a1b19822798a3d21f9e56ca90
unspecified for later <50639497+Pr0mptGithub@users.noreply.github.com>
2020-10-22 20:26:32 +0100

    Add files via upload

---
+++ horrorshow.py
@@ -0,0 +1,206 @@
+x=1
+F="Fizz"
+B="Buzz"
+while x <= 100: # lets begin the horror show :)
+	if x == 1:
+		print(x)
+	elif x == 2:
+		print(x)
+	elif x == 3:
+		print(F)
+	elif x == 4:
+		print(x)
+	elif x == 5:
+		print(B)
+	elif x == 6:
+		print(F)
+	elif x == 7:
+		print(x)
+	elif x == 8:
+		print(x)
+	elif x == 9:
+		print(F)
+	elif x == 10:
+		print(B)
+	elif x == 11:
+		print(x)
+	elif x == 12:
+		print(F)
+	elif x == 13:
+		print(x)
+	elif x == 14:
+		print(x)
+	elif x == 15:
+		print(F, B)
+	elif x == 16:
+		print(x)
+	elif x == 17:
+		print(x)
+	elif x == 18:
+		print(F)
+	elif x == 19:
+		print(x)
+	elif x == 20:
+		print(B)
+	elif x == 21:
+		print(F)
+	elif x == 22:
+		print(x)
+	elif x == 23:
+		print(x)
+	elif x == 24:
+		print(F)
+	elif x == 25:
+		print(B)
+	elif x == 26:
+		print(x)
+	elif x == 27:
+		print(F)
+	elif x == 28:
+		print(x)
+	elif x == 29:
+		print(x)
+	elif x == 30:
+		print(F, B)
+	elif x == 31:
+		print(x)
+	elif x == 32:
+		print(x)
+	elif x == 33:
+		print(F)
+	elif x == 34:
+		print(x)
+	elif x == 35:
+		print(B)
+	elif x == 36:
+		print(F)
+	elif x == 37:
+		print(x)
+	elif x == 38:
+		print(x)
+	elif x == 39:
+		print(F)
+	elif x == 40:
+		print(B)
+	elif x == 41:
+		print(x)
+	elif x == 42:
+		print(F)
+	elif x == 43:
+		print(x)
+	elif x == 44:
+		print(x)
+	elif x == 45:
+		print(F, B)
+	elif x == 46:
+		print(x)
+	elif x == 47:
+		print(x)
+	elif x == 48:
+		print(F)
+	elif x == 49:
+		print(x)
+	elif x == 50:
+		print(B)
+	elif x == 51:
+		print(F)
+	elif x == 52:
+		print(x)
+	elif x == 53:
+		print(x)
+	elif x == 54:
+		print(F)
+	elif x == 55:
+		print(B)
+	elif x == 56:
+		print(x)
+	elif x == 57:
+		print(F)
+	elif x == 58:
+		print(x)
+	elif x == 59:
+		print(x)
+	elif x == 60:
+		print(F, B)
+	elif x == 62:
+		print(x)
+	elif x == 63:
+		print(F)
+	elif x == 64:
+		print(x)
+	elif x == 65:
+		print(B)
+	elif x == 66:
+		print(F)
+	elif x == 67:
+		print(x)
+	elif x == 68:
+		print(x)
+	elif x == 69:
+		print(F)
+	elif x == 70:
+		print(B)
+	elif x == 71:
+		print(x)
+	elif x == 72:
+		print(F)
+	elif x == 73:
+		print(x)
+	elif x == 74:
+		print(x)
+	elif x == 75:
+		print(F, B)
+	elif x == 76:
+		print(x)
+	elif x == 77:
+		print(x)
+	elif x == 78:
+		print(F)
+	elif x == 79:
+		print(x)
+	elif x == 80:
+		print(B)
+	elif x == 81:
+		print(F)
+	elif x == 82:
+		print(x)
+	elif x == 83:
+		print(x)
+	elif x == 84:
+		print(F)
+	elif x == 85:
+		print(B)
+	elif x == 86:
+		print(x)
+	elif x == 87:
+		print(F)
+	elif x == 88:
+		print(x)
+	elif x == 89:
+		print(x)
+	elif x == 90:
+		print(F, B)
+	elif x == 91:
+		print(x)
+	elif x == 92:
+		print(x)
+	elif x == 93:
+		print(F)
+	elif x == 94:
+		print(x)
+	elif x == 95:
+		print(B)
+	elif x == 96:
+		print(F)
+	elif x == 97:
+		print(x)
+	elif x == 98:
+		print(x)
+	elif x == 99:
+		print(F)
+	elif x == 100:
+		print(B)
+	elif x > 100:
+		print("how did you get here")
+	x+=1
+	
